216595fa85c0c928ec6ce6da076cf67fa8db62c5
Vincent Jauneau <vincent.jauneau@platypus-creation.com>
2011-02-16 20:10:14 +0100

    Fixed a bug on /give commamd when trying to get a material with a composed material name (ie: lava bucket). Refactored build.xml.

--- src/main/java/com/dinnerbone/bukkit/scrap/ScrapBukkit.java
+++ src/main/java/com/dinnerbone/bukkit/scrap/ScrapBukkit.java
@@ -2,18 +2,19 @@
 package com.dinnerbone.bukkit.scrap;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.material.MaterialData;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,6 +23,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Miscellaneous administrative commands
  *
  * @author Dinnerbone
+ * @author Jauneau Vincent
  */
 public class ScrapBukkit extends JavaPlugin {
 
@@ -139,7 +141,7 @@ public class ScrapBukkit extends JavaPlugin {
     }
 
     private boolean performGive(CommandSender sender, String[] split) {
-        if ((split.length > 3) || (split.length == 0)) {
+        if (split.length == 0) {
             return false;
         }
         if (!sender.isOp()) {
@@ -149,44 +151,62 @@ public class ScrapBukkit extends JavaPlugin {
 
         Player player = null;
         Material material = null;
+        
+        ArrayList<String> args = new ArrayList<String>();
+        args.addAll(Arrays.asList(split));
+
+        // Get the count
         int count = 1;
-        String[] gData = null;
-        Byte bytedata = null;
-        if (split.length >= 1) {
-            gData = split[0].split(":");
-            material = Material.matchMaterial(gData[0]);
-            if (material == null) {
-            	material = getMaterial(gData[0]);
-            }
-            if (gData.length == 2) {
-                bytedata = Byte.valueOf(gData[1]);
-            }
+        if (args.size() > 1) {
+	        try {
+	            count = Integer.parseInt(args.get(args.size()-1)); // /give material name 42
+	            args.remove(args.size()-1);
+	        } catch (NumberFormatException ex) {
+	        	try {
+	                count = Integer.parseInt(args.get(args.size()-2)); // /give material name 42 player
+	                
+	                // Get the player
+	                player = getServer().getPlayer(args.get(args.size()-1));
+	                if (player == null) {
+	                    sender.sendMessage(ChatColor.RED + "'" + split[split.length-1] + "' is not a valid player!");
+	                    return false;
+	                }
+		            args.remove(args.size()-1);
+		            args.remove(args.size()-1);
+	        	} catch (NumberFormatException e) {}
+	        }
         }
-        if (split.length >= 2) {
-            try {
-                count = Integer.parseInt(split[1]);
-            } catch (NumberFormatException ex) {
-                sender.sendMessage(ChatColor.RED + "'" + split[1] + "' is not a number!");
-                return false;
-            }
-        }
-        if (split.length == 3) {
-            player = getServer().getPlayer(split[2]);
-            if (player == null) {
-                sender.sendMessage(ChatColor.RED + "'" + split[2] + "' is not a valid player!");
-                return false;
-            }
-        } else {
+
+        // Set sender as player if no player was specified 
+        if (player == null) {
             if (anonymousCheck(sender)) {
                 return false;
             } else {
                 player = (Player) sender;
             }
         }
+
+        // Get the Metadata
+        String[] data = args.get(args.size()-1).split(":");
+        Byte bytedata = null;
+        if (data.length == 2) {
+            bytedata = Byte.valueOf(data[1]);
+        }
+        args.set(args.size()-1, data[0]);
+
+        // Get the Material
+        String materialName = "";
+        for (String s : args) materialName += s + " ";
+        materialName = materialName.trim();
+        material = Material.matchMaterial(materialName);
         if (material == null) {
-            sender.sendMessage(ChatColor.RED + "Unknown item");
+        	material = getMaterial(materialName);
+        }
+        if (material == null) {
+            sender.sendMessage(ChatColor.RED + "Unknown item \"" + materialName + "\"");
             return false;
         }
+
         if (bytedata != null) {
             player.getInventory().addItem(new ItemStack(material, count, (short) 0, bytedata));
         } else {
